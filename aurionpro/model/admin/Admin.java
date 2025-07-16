e3e5ef8b379bf9774aad1c45d43e23e0cd69e08d
RanePrathamesh <130394122+RanePrathamesh@users.noreply.github.com>
2025-07-16 10:01:29 +0530

    Add files via upload

---
+++ aurionpro/model/admin/Admin.java
@@ -0,0 +1,13 @@
+package com.aurionpro.model.admin;
+
+public class Admin {
+    private String name;
+
+    public Admin(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
---
+++ aurionpro/model/food/MainCourse.java
@@ -0,0 +1,18 @@
+package com.aurionpro.model.food;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainCourse implements IFoodType{
+
+	@Override
+	public String getFoodType() {
+		return "MainCourse";
+		
+	}
+	
+	
+
+	
+
+}
